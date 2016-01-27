a7a030f163951a45251cd68265fcf8e23d263c0f
Thoth19 <kpseipp@gmail.com>
2016-01-26 23:59:12 -0800

    Not perfect, but should find the one with min error
    and save that one as a file.

--- sa.decision_tree.py
+++ sa.decision_tree.py
@@ -5,6 +5,7 @@ import pydot
 import random
 from sklearn import tree
 from sklearn.externals.six import StringIO  
+import datetime
 
 # Load Data
 # Note that I removed the top line so we could pull the data 
@@ -17,6 +18,9 @@ Y = data[0:3351, 1000]
 test_X = data[3351:, :1000]
 test_Y = data[3351:, 1000]
 
+ans_data = np.loadtxt('testing_data2.txt', delimiter='|')
+ans_X = ans_data[:, :1000]
+
 # For printing the decision trees themselves
 dot_data = StringIO() 
 
@@ -27,75 +31,49 @@ training_loss_list = []
 test_loss_list = []
 training_loss_list_normalized = []
 test_loss_list_normalized = []
-for i in range(1,26):
+
+lowest_loss = 1
+
+for i in range(1,100):
     clf = tree.DecisionTreeClassifier()
     clf.min_samples_leaf = i
     samples_list.append(i)
     print i
     clf.fit(X,Y)
-    # We can find the classifcation error using the score
-    # function (which is accuracy) and multiply by the sample size
-    training_loss_list.append(400*(1 - clf.score(X,Y)))
-    test_loss_list.append(169*(1 - clf.score(test_X, test_Y)))
     # However, we likely also want it normalized.
     training_loss_list_normalized.append(1 - (clf.score(X,Y)))
     test_loss_list_normalized.append(1 - (clf.score(test_X, test_Y)))
+    if test_loss_list_normalized[-1] < lowest_loss:
+        lowest_loss = test_loss_list_normalized[-1]
+        best_clf = clf
 
-    # If we want to print out the tree as a diagram
-    # tree.export_graphviz(clf, out_file=dot_data) 
-    # graph = pydot.graph_from_dot_data(dot_data.getvalue()) 
-    # graph.write_pdf("training{}.pdf".format(i))
-
-# fig, ax = pyplot.subplots()
-# l1, = pyplot.plot(samples_list, training_loss_list, label="Training")
-# l2, = pyplot.plot(samples_list, test_loss_list, label="Testing")
-# legend = ax.legend(loc='upper center', shadow=True)
-# pyplot.title('Leaf Node Size vs Classification Error')
-# pyplot.show()
-fig, ax = pyplot.subplots()
-l1 = pyplot.plot(samples_list, training_loss_list_normalized, label="Training")
-l2 = pyplot.plot(samples_list, test_loss_list_normalized, label="Testing")
-legend = ax.legend(loc='upper center', shadow=True)
-pyplot.title('Leaf Node Size vs Classification Error (Normalized)')
-pyplot.show()
+training_loss_list_normalized.append("B")
+test_loss_list_normalized.append("B")
+samples_list.append("B")
 
 # PART B
 # Now we want to do the same thing, but instead modifying the maximal
 # tree depth. 
 
-depth_list = []
-training_loss_list = []
-test_loss_list = []
-training_loss_list_normalized = []
-test_loss_list_normalized = []
-for i in range(2,21):
+for i in range(2,100):
     print i
     clf = tree.DecisionTreeClassifier()
     clf.max_depth = i
-    depth_list.append(i)
+    samples_list.append(i)
     clf.fit(X,Y)
-    # We can find the classifcation error using the score
-    # function (which is accuracy) and multiply by the sample size
-    training_loss_list.append(400*(1 - clf.score(X,Y)))
-    test_loss_list.append(169*(1 - clf.score(test_X, test_Y)))
+
     # However, we likely also want it normalized.
     training_loss_list_normalized.append(1 - (clf.score(X,Y)))
     test_loss_list_normalized.append(1 - (clf.score(test_X, test_Y)))
+    if test_loss_list_normalized[-1] < lowest_loss:
+        lowest_loss = test_loss_list_normalized[-1]
+        best_clf = clf
 
-    # If we want to print out the tree as a diagram
-    # tree.export_graphviz(clf, out_file=dot_data) 
-    # graph = pydot.graph_from_dot_data(dot_data.getvalue()) 
-    # graph.write_pdf("training{}.pdf".format(i))
-
-# fig, ax = pyplot.subplots()
-# l1, = pyplot.plot(depth_list, training_loss_list, label="Training")
-# l2, = pyplot.plot(depth_list, test_loss_list, label="Testing")
-# legend = ax.legend(loc='upper center', shadow=True)
-# pyplot.title('Maximum Depth vs Classification Error')
-# pyplot.show()
-fig, ax = pyplot.subplots()
-l1 = pyplot.plot(depth_list, training_loss_list_normalized, label="Training")
-l2 = pyplot.plot(depth_list, test_loss_list_normalized, label="Testing")
-legend = ax.legend(loc='upper center', shadow=True)
-pyplot.title('Maximum Depth vs Classification Error (Normalized)')
-pyplot.show()
+# Once we are done, we want to put the optimal thing in a solution set.
+# We also want the name to be unique
+ans_Y = best_clf.predict(ans_X)
+f = open("solution_{}_{}".format(lowest_loss, str(datetime.datetime.now())), 'w')
+f.write("Id,Prediction\n")
+for i,j in enumerate(ans_Y):
+    f.write("{},{}\n".format(i+1,int(j)))
+#we really like number 56 from the depth structure idk why
