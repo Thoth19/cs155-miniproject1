a5e2f42fce6cf0d33c07c516bc6ddcdf9066f7b2
Thoth19 <kpseipp@gmail.com>
2016-01-27 23:47:13 -0800

    We have ADA boosting, but it's kinda bad.
    This is significantly lower than the RNG sorta
    thing that we did earlier. Maybe I should make
    the trees only depth one like the default

--- sa.decision_tree.py
+++ sa.decision_tree.py
@@ -4,6 +4,8 @@ import numpy as np
 import pydot 
 import random
 from sklearn import tree
+from sklearn import cross_validation
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.externals.six import StringIO  
 import datetime
 
@@ -17,62 +19,24 @@ Y = data[0:3351, 1000]
 
 test_X = data[3351:, :1000]
 test_Y = data[3351:, 1000]
+X, test_X, Y, test_Y = cross_validation.train_test_split(data[:, :1000], data[:,1000],test_size=0.4)
 
 ans_data = np.loadtxt('testing_data2.txt', delimiter='|')
 ans_X = ans_data[:, :1000]
 
-# For printing the decision trees themselves
-dot_data = StringIO() 
+clf = AdaBoostClassifier(n_estimators=10000, base_estimator=tree.DecisionTreeClassifier(compute_importances=None, criterion='gini',
+            max_depth=10, max_features=None, min_density=None,
+            min_samples_leaf=1, min_samples_split=2, random_state=None,
+            splitter='best'))
+clf.fit(X,Y)
 
-# PART A
+loss = 1 - clf.score(test_X, test_Y)
 
-samples_list = []
-training_loss_list = []
-test_loss_list = []
-training_loss_list_normalized = []
-test_loss_list_normalized = []
 
-lowest_loss = 1
-
-for i in range(1,100):
-    clf = tree.DecisionTreeClassifier()
-    clf.min_samples_leaf = i
-    samples_list.append(i)
-    print i
-    clf.fit(X,Y)
-    # However, we likely also want it normalized.
-    training_loss_list_normalized.append(1 - (clf.score(X,Y)))
-    test_loss_list_normalized.append(1 - (clf.score(test_X, test_Y)))
-    if test_loss_list_normalized[-1] < lowest_loss:
-        lowest_loss = test_loss_list_normalized[-1]
-        best_clf = clf
-
-training_loss_list_normalized.append("B")
-test_loss_list_normalized.append("B")
-samples_list.append("B")
-
-# PART B
-# Now we want to do the same thing, but instead modifying the maximal
-# tree depth. 
-
-for i in range(2,100):
-    print i
-    clf = tree.DecisionTreeClassifier()
-    clf.max_depth = i
-    samples_list.append(i)
-    clf.fit(X,Y)
-
-    # However, we likely also want it normalized.
-    training_loss_list_normalized.append(1 - (clf.score(X,Y)))
-    test_loss_list_normalized.append(1 - (clf.score(test_X, test_Y)))
-    if test_loss_list_normalized[-1] < lowest_loss:
-        lowest_loss = test_loss_list_normalized[-1]
-        best_clf = clf
-
-# Once we are done, we want to put the optimal thing in a solution set.
+# Once we are done, we want to put the thing in a solution set.
 # We also want the name to be unique
-ans_Y = best_clf.predict(ans_X)
-f = open("solution_{}_{}".format(lowest_loss, str(datetime.datetime.now())), 'w')
+ans_Y = clf.predict(ans_X)
+f = open("solution_{}_{}".format(loss, str(datetime.datetime.now())), 'w')
 f.write("Id,Prediction\n")
 for i,j in enumerate(ans_Y):
     f.write("{},{}\n".format(i+1,int(j)))
